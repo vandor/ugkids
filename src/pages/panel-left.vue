bec8af541baa70040a2b5f37655c8360175fe758
Josh Loya <vandor@users.noreply.github.com>
2018-02-19 17:22:13 -0500

    Change title in left side-bar

--- src/pages/panel-left.vue
+++ src/pages/panel-left.vue
@@ -1,7 +1,7 @@
 <template>
   <f7-page>
     <f7-navbar title=""></f7-navbar>
-    <f7-block-title>Pages</f7-block-title>
+    <f7-block-title>First-Time Registration</f7-block-title>
     <f7-list>
       <f7-list-button popup-open="#register-adult" title="Register Adult" panel-close></f7-list-button>
       <f7-list-button popup-open="#register-kid" title="Register Kids" panel-close></f7-list-button>
