6a5561f01d9715bcf8aa47a691b3b3133539b2b1
Josh Loya <jloya3@oreillyauto.com>
2018-03-05 16:27:38 -0500

    Add authentication link and route to Auth0 login app

--- src/pages/checkin.vue
+++ src/pages/checkin.vue
@@ -5,6 +5,9 @@
         <f7-link icon-if-ios="f7:menu" icon-if-md="material:menu" panel-open="left"></f7-link>
       </f7-nav-left>
       <f7-nav-title>Underground Kids</f7-nav-title>
+      <f7-nav-right>
+        <f7-link @click="login()">Login</f7-link>
+      </f7-nav-right>
     </f7-navbar>
     <f7-block inset>
       <iframe class="airtable-embed airtable-dynamic-height"
@@ -15,6 +18,24 @@
   </f7-page>
 </template>
 <script>
-export default {}
+import AuthService from '../auth/AuthService'
+const auth = new AuthService()
+const { login, logout, authenticated, authNotifier } = auth
+
+export default {
+  data () {
+    authNotifier.on('authChange', authState => {
+      this.authenticated = authState.authenticated
+    })
+    return {
+      auth,
+      authenticated,
+    }
+  },
+  methods: {
+    login,
+    logout
+  }
+}
 </script>
 
