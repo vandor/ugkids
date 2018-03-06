2658632f4d758f2b7a845de6dcd0b793cfc0eb6a
Josh Loya <jloya3@oreillyauto.com>
2018-03-06 14:32:55 -0500

    Add middleware so express hands down requests to the framework7 router

--- server.js
+++ server.js
@@ -1,9 +1,13 @@
 var express = require('express');
 var path = require('path');
 var serveStatic = require('serve-static');
+var history = require('connect-history-api-fallback');
 var port = process.env.PORT || 8080;
 
 app = express();
-app.use(serveStatic(path.join(__dirname, '/www')));
+const staticMiddleware = serveStatic(path.join(__dirname, '/www'));
+app.use(staticMiddleware);
+app.use(history({}));
+app.use(staticMiddleware);
 
 app.listen(port, () => console.log('server listening on port ' + port));
