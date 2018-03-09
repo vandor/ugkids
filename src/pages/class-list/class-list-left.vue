1f510054984a0122ec73f9a8359e3abb73a8d191
Josh Loya <jloya3@oreillyauto.com>
2018-03-09 18:08:08 -0500

    Style class list sidebar as links with arrow icon

--- src/pages/class-list/class-list-left.vue
+++ src/pages/class-list/class-list-left.vue
@@ -3,10 +3,10 @@
     <f7-navbar title=""></f7-navbar>
     <f7-block-title>Kids Classes</f7-block-title>
     <f7-list>
-      <f7-list-button href="/class-list/nursery" title="Nursery" view=".view-main" panel-close></f7-list-button>
-      <f7-list-button href="/class-list/preschool" title="Preschool" view=".view-main" panel-close></f7-list-button>
-      <f7-list-button href="/class-list/jr-elementary" title="Jr Elementary" view=".view-main" panel-close></f7-list-button>
-      <f7-list-button href="/class-list/elementary" title="Elementary" view=".view-main" panel-close></f7-list-button>
+      <f7-list-item link="/class-list/nursery" title="Nursery" view=".view-main" panel-close></f7-list-item>
+      <f7-list-item link="/class-list/preschool" title="Preschool" view=".view-main" panel-close></f7-list-item>
+      <f7-list-item link="/class-list/jr-elementary" title="Jr Elementary" view=".view-main" panel-close></f7-list-item>
+      <f7-list-item link="/class-list/elementary" title="Elementary" view=".view-main" panel-close></f7-list-item>
     </f7-list>
   </f7-page>
 </template>
