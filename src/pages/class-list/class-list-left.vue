c433e2ebcc75c5eca589d84d874366bfcd617db6
Josh Loya <jloya3@oreillyauto.com>
2018-03-10 13:27:25 -0500

    Add logo to class-list sidebar

--- src/pages/class-list/class-list-left.vue
+++ src/pages/class-list/class-list-left.vue
@@ -1,6 +1,12 @@
 <template>
   <f7-page>
     <f7-navbar title=""></f7-navbar>
+    <f7-block strong inset>
+      <div class="logo">
+        <img src="../../img/ugkids.jpg" alt="banner"/>
+      </div>
+    </f7-block>
+
     <f7-block-title>Kids Classes</f7-block-title>
     <f7-list>
       <f7-list-item link="/class-list/nursery" title="Nursery" view=".view-main" panel-close></f7-list-item>
@@ -13,3 +19,13 @@
 <script>
 export default {}
 </script>
+<style scoped>
+.logo {
+  display: flex;
+  justify-content: center;
+  align-items: center;
+}
+.logo img {
+  max-width: 100%;
+}
+</style>
