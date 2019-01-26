2a08e2f6c5bd62f815ee832b3bd75f7c07bb2e2b
ericunderground <47044687+ericunderground@users.noreply.github.com>
2019-01-26 13:02:40 -0500

    Updated view link to hit new Airtable

--- src/pages/class-list/jr-elementary/jr-elementary-gallery.vue
+++ src/pages/class-list/jr-elementary/jr-elementary-gallery.vue
@@ -1,5 +1,5 @@
 <template>
-  <ugkids-class-gallery title="Jr Elementary Class" airtable-share-id="shrmsZ3nyvztprO3b"></ugkids-class-gallery>
+  <ugkids-class-gallery title="Jr Elementary Class" airtable-share-id="shraWqJLtS8I99zSa"></ugkids-class-gallery>
 </template>
 <script>
 import ClassGalleryComponent from '../../class-gallery.vue';
