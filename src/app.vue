8941a34f6522c8e8d99fb15b1e5569db178a5e82
ericunderground <47044687+ericunderground@users.noreply.github.com>
2019-01-25 20:46:49 -0500

    Changed SRC of Adult Registration to correct form
    Turns out I linked the straight-up Airtable View last time, not the form. Oops.

--- src/app.vue
+++ src/app.vue
@@ -18,7 +18,7 @@
         </f7-navbar>
         <f7-block inset>
           <iframe class="airtable-embed airtable-dynamic-height"
-            src="https://airtable.com/embed/shrJSzpZFdrinTt15?backgroundColor=gray"
+            src="https://airtable.com/embed/shrVw4scukcdCJt6l?backgroundColor=gray"
             frameborder="0" onmousewheel="" width="100%" height="1100"
             style="background: transparent; border-width: 0;"></iframe>
         </f7-block>
